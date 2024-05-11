49c47d8d9975194f0f3aa86abb3a3fa7f2497494
huiju jang <gmlwn6316@gmail.com>
2024-05-11 17:50:30 +0900

    8commit

--- SWEAT_D2/1974(다시).py
+++ SWEAT_D2/1974(다시).py
@@ -1,27 +1,37 @@
-T=int(input())
-for test_case in range(1,T+1):
+T = int(input())
+
+for test_case in range(1, T + 1):
     sdoku=[]
     for _ in range(9):
         sdoku.append(list(map(int,input().split())))
+    #중복 있을때 0 없을때 1
     def check_sdoku():
         for i in range(9):
             row_num=[0]*10
             column_num=[0]*10
             for j in range(9):
+                #가로검사
                 row=sdoku[i][j]
+                #세로검사
                 column=sdoku[j][i]
-                if row_num[row]: return 0
-                if column_num[column]: return 0
+
+                #중복된 숫자가 있을때
+                if row_num[row]==1: return 0
+                if column_num[column]==1: return 0
+				
                 row_num[row]=1
                 column_num[column]=1
-            if i%3==0 and j%3==0:
-                temp=[0]*10
-                for k in range(i,3+i):
-                    for s in range(j,j+3):
-                        if temp[data[k][s]]:
-                            return 0
-                        temp[k][s]=1
+
+                #3x3행렬일때 중복검사
+                if i%3==0 and j%3==0:
+                    temp=[0]*10
+                    for k in range(i,3+i):
+                        for s in range(j,3+j):
+                            num=sdoku[k][s]
+                            if temp[num]==1:
+                                return 0
+                            temp[num]=1
         return 1
-    result=0
-    result=check_sdoku()
-    print('#{} {}'.format(test_case,result))
+    check=check_sdoku()
+    print('#{} {}'.format(test_case,check))
+    
